a8050cf159d8379170dbcc7362ae0e02dfce8dc1
Akruzen <1omkarphadke@gmail.com>
2022-09-07 15:17:05 +0530

    Added basic logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+class StringCalculator {
+
+    private boolean isANumber (String num) {
+        try {
+            Integer.parseInt(num);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public int add (String numbers) {
+        int sum = 0;
+        String[] arr = numbers.split(",");
+        for (int i = 0; i < arr.length; i++) {
+            if (isANumber(arr[i].trim())) {
+                sum += Integer.parseInt(arr[i].trim());
+            } else {
+                sum += (arr[i].trim().charAt(0) - 96);
+            }
+        }
+        return sum;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringCalculator calculator = new StringCalculator();
+        System.out.println(calculator.add("1, 2, a"));
     }
 }
