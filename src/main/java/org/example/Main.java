fa4b8e64f151296575ca12aba92f1a62614cd57f
Akruzen <1omkarphadke@gmail.com>
2022-09-07 15:35:43 +0530

    Added negative number support

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.List;
+
 class StringCalculator {
 
     private boolean isANumber (String num) {
@@ -10,12 +12,21 @@ class StringCalculator {
             return false;
         }
     }
-    public int add (String numbers) {
+
+    private boolean isNegative (int num) {
+        return num < 0;
+    }
+    public int add (String numbers) throws Exception {
         int sum = 0;
         String[] arr = numbers.split(",");
         for (int i = 0; i < arr.length; i++) {
             if (isANumber(arr[i].trim())) {
-                sum += Integer.parseInt(arr[i].trim());
+                if (isNegative(Integer.parseInt(arr[i].trim()))) {
+                    throw new Exception("Negatives not allowed: " + arr[i].trim());
+                }
+                else {
+                    sum += Integer.parseInt(arr[i].trim());
+                }
             } else {
                 sum += (arr[i].trim().charAt(0) - 96);
             }
@@ -25,8 +36,8 @@ class StringCalculator {
 }
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StringCalculator calculator = new StringCalculator();
-        System.out.println(calculator.add("1, 2, a"));
+        System.out.println(calculator.add("1, 2, a, c"));
     }
 }
